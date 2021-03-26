132870c317a92198dc67812f9d3387d26359a352
PhucNVH <nguyenvuhoangphuc11@gmail.com>
2021-03-26 14:35:42 +0700

    add simple classification

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -3,17 +3,25 @@
  * Licensed under the MIT License.
  */
 
-
-module.exports = function (controller)  {
-
+module.exports = function (controller) {
   const axios = require("axios");
   const server = process.env.SERVER_URI;
+
   const saveMessage = async (message) => {
-    return await axios.post(`${server}/save-message`,{text:message.text,username:message.user,message_type:message.type,conversation_id:message.conversation?message.conversation.id:message.reference.conversation.id});
-  }
+    return await axios
+      .post(`${server}/save-message`, {
+        text: message.text,
+        username: message.user,
+        message_type: message.type,
+        conversation_id: message.conversation
+          ? message.conversation.id
+          : message.reference.conversation.id,
+      })
+      .catch((e) => console.log(e));
+  };
 
   controller.middleware.receive.use(async (bot, message, next) => {
-    if(message.type==='message'){
+    if (message.type === "message") {
       await saveMessage(message);
     }
     message.logged = true;
@@ -21,27 +29,46 @@ module.exports = function (controller)  {
   });
 
   controller.middleware.send.use(async (bot, message, next) => {
-    if(message.type==='message'){
-      message.user="bot";
-      await saveMessage(message);
+    if (message.type === "message") {
+      message.user = "bot";
+      saveMessage(message);
     }
-    message.logged = true; 
+    message.logged = true;
     next();
   });
 
   controller.on("load_message", async (bot, message) => {
-    const val = await axios.post(`${server}/load-conversation`,{conversation_id: message.user});
-    if(val.data.messages){
-        await bot.reply(message,{messages:val.data.messages,type: 'load_message'});
+    const val = await axios.post(`${server}/load-conversation`, {
+      conversation_id: message.user,
+    });
+    if (val.data.messages) {
+      await bot.reply(message, {
+        messages: val.data.messages,
+        type: "load_message",
+      });
     }
   });
 
   controller.on("feedback", async (bot, message) => {
-    await axios.post(`${server}/add-feedback`,{conversation_id: message.reference.conversation.id,feedback: message.text,index:message.index});
+    await axios.post(`${server}/add-feedback`, {
+      conversation_id: message.reference.conversation.id,
+      feedback: message.text,
+      index: message.index,
+    });
   });
 
   controller.on("message,direct_message", async (bot, message) => {
-    await bot.reply(message, `Echo: ${message.text}`);
+    console.log(message);
+    const resp = await axios
+      .post(`${server}/get-response`, {
+        conversation_id: message.reference.conversation.id,
+        message: message.text,
+      })
+      .then((res) => res.data)
+      .catch((e) => console.log(e));
+    if (resp) {
+      console.log(resp);
+      bot.reply(message, { text: resp.response });
+    }
   });
-
 };
